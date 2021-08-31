0d02ae6ede7778834b48a5e374c1a7d6338b9d81
eldadmwangi <eldadmwangi@gmail.com>
2021-08-31 12:32:55 +0300

    added a little bit of styling to the app

--- src/components/FetchJokes.js
+++ src/components/FetchJokes.js
@@ -30,14 +30,16 @@ function FetchJokes() {
 
     return (
         <>
+        <h3 className='app__name'style={{display:'flex',marginLeft:'20px',fontFamily:'cursive',display:'flex',justifyContent:'center'}} >Jokes App</h3>
+        <h4 className='header'style={{display:'flex',marginLeft:'20px',fontFamily:'cursive'}}  >Select category to get respective joke</h4>
         <div className ='fetch__jokes'>
         <div className='jokes__container'>
-        <select className='select__option' value={currentCategory} onChange={(e)=>setcurrentCategory(e?.target?.value)}>
+        <select className='select__option' style={{fontSize:'16px',fontFamily:'serif'}} value={currentCategory} onChange={(e)=>setcurrentCategory(e?.target?.value)}>
             {categoryData.map((category)=>(
-                <option key={category} value={category} > {category} </option>
+                <option className='option' style={{backgroundColor:'grey'}} key={category} value={category} > {category} </option>
             ))}  
         </select>
-                <div className="joke__container">{joke.value}</div>
+                <div className="joke__container" style={{fontFamily:'cursive'}}>{joke?.value}</div>
             </div>
             </div>
             </>
